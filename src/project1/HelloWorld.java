116ac4c3582813e40e482f1be8f9afa82320df34
23jy0217 <23jy0217@PC18.jynet2.jec.ac.jp>
2024-10-16 12:36:03 +0900

    iを表示するプログラム

--- src/project1/HelloWorld.java
+++ src/project1/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("HelloWorld!");
+		
+		int i;
+		i = 0;
+		System.out.println(i);
 	}
 
 }
