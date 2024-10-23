7341f61060f4767d1b9f6656e1837abdc0b239bd
shiina <23jy0217@jec.ac.jp>
2024-10-23 12:24:36 +0900

    正規の形にした

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public class Hello
-  public static void main(String[] args{
+public class Hello{
+  public static void main(String[] args){
     System.out.println("HelloWorld");
     System.out.println("HelloWorld");
     System.out.println("HelloWorld");
@@ -7,7 +7,7 @@ public class Hello
     int a,b,c;
     double x,y,z;
   }
+}
 
   git config --global user.name 'shiina'
   git config --global user.email '23jy0217@jec.ac.jp'
-  
